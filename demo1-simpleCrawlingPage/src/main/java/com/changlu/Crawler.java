8d558411589464fc608fc7979d5c7a44c5d7e960
changlua <xiaoxiaochanglu@163.com>
2021-11-02 17:03:06 +0800

    demo1

---
+++ demo1-simpleCrawlingPage/src/main/java/com/changlu/Crawler.java
@@ -0,0 +1,41 @@
+package com.changlu;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.logging.Logger;
+
+/**
+ * @ClassName Crawler
+ * @Author ChangLu
+ * @Date 2021/10/30 22:43
+ * @Description TODO
+ */
+public class Crawler {
+
+    public static Logger logger = Logger.getLogger("com.changlu");
+
+    public static void main(String[] args) throws IOException {
+        logger.info("进行请求中...");
+        Document document = Jsoup.connect("https://github.com/freefq/free").get();
+        logger.info("开始解析...");
+        String content  = document.getElementsByTag("code").eq(2).text();
+        logger.info("解析成功...");
+        writeFile(content);
+    }
+
+    public static void writeFile(String content){
+        try {
+            logger.info("写入文件中....");
+            BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream("code.txt"));
+            bos.write(content.getBytes());
+            logger.info("写入成功！");
+            bos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ demo1-simpleCrawlingPage/打包/startup.vbs
@@ -0,0 +1,2 @@
+set objshell=createobject("wscript.shell")
+objshell.run("%comspec% /k java -jar Getv2ray.jar")
